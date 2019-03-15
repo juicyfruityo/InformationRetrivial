ae966188568a63d9a43bd30f092415d16dab2afa
juicyfruityo <pooploorus@gmail.com>
2019-03-15 20:48:44 +0300

    add 1st hw files

---
+++ sekitei(hw1)/HA_1/extract_features.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+import sys
+# you may add imports if needed (and if they are installed)
+
+
+def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
+    print >> sys.stderr, "Not implemented"
---
+++ sekitei(hw1)/HA_2/sekitei_segments.py
@@ -0,0 +1,26 @@
+ # coding: utf-8
+
+
+import sys
+import os
+import re
+import random
+import time
+#from sklearn.cluster import <any cluster algorithm>
+import numpy
+
+
+sekitei = None;
+
+
+def define_segments(QLINK_URLS, UNKNOWN_URLS, QUOTA):
+    print "define_segments is not implemented";
+
+
+#
+# returns True if need to fetch url
+#
+def fetch_url(url):
+    #global sekitei
+    #return sekitei.fetch_url(url);
+    return True;
